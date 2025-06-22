d2f33b672e948ed0e452cf4b6c02e9f79b1403f1
DMiniKim <angks1212@naver.com>
2025-06-23 00:58:16 +0900

    (2025.6.23) 제작 3일차

--- Look_and_Shoot/Look_and_Shoot/Title.cpp
+++ Look_and_Shoot/Look_and_Shoot/Title.cpp
@@ -5,14 +5,34 @@
 
 void Title::Init()
 {
-	cout << "Title Init";
+
+shapeArr[0] = " __               _   ";
+shapeArr[1] = "|  |    ___  ___ | |_ ";
+shapeArr[2] = "|  |__ | . || . || '_|";
+shapeArr[3] = "|_____||___||___||_,_|";
+
+shapeArr[4] = " _____         _ ";
+shapeArr[5] = "|  _  | ___  _| |";
+shapeArr[6] = "|     ||   || . |";
+shapeArr[7] = "|__|__||_|_||___|";
+
+
+shapeArr[8]  = " _____  _              _   ";
+shapeArr[9]  = "|   __|| |_  ___  ___ | |_ ";
+shapeArr[10] = "|__   ||   || . || . ||  _|";
+shapeArr[11] = "|_____||_|_||___||___||_|  ";
+
+
 }
 
 void Title::Update()
 {
 	// Player  해결 해야함
-	DoubleBuffer::GetInstance()->WriteBuffer(15, 15,"Title", 13);
 
+	for (auto i = 0; i < 12; i++)
+	{															
+	    DoubleBuffer::GetInstance()->WriteBuffer(9, i + 9 , shapeArr[i],    9);
+	}
 	if (GetAsyncKeyState(VK_RETURN))
 	{
 		SceneManager::GetInstance()->SetScene(STAGE);
--- Look_and_Shoot/Look_and_Shoot/Title.h
+++ Look_and_Shoot/Look_and_Shoot/Title.h
@@ -2,6 +2,8 @@
 #include "Scene.h"
 class Title : public Scene
 {
+private:
+	const char* shapeArr[12];
 public:
 	virtual void Init() override;
 	virtual void Update() override;
